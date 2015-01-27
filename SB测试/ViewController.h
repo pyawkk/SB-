1f4c05b25b10c320790e52b59fc105a36fbf2270
pyawkk <624204727@qq.com>
2015-01-27 11:43:39 +0800

    Initial Commit

---
+++ SB测试/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SB测试
+//
+//  Created by panyong on 15/1/27.
+//  Copyright (c) 2015年 monileNowGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SB测试/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SB测试
+//
+//  Created by panyong on 15/1/27.
+//  Copyright (c) 2015年 monileNowGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
